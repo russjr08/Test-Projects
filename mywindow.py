cc208f06cb193a012727a0b3884781b97a5c9bd4
Russell Richardson <russjr08@gmail.com>
2011-05-21 13:26:08 -0500

    Added button and cleaned up program

--- mywindow.py
+++ mywindow.py
@@ -18,8 +18,15 @@ class Base:
         # Setting up the Window object and it's settings
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_size_request(600, 100)
-        self.window.set_position(gtk.WIN_POS_CENTER)   
-        self.window.show() # Show the window
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        self.button1 = gtk.Button("Exit") # Define a new button
+        self.button1.connect("clicked", self.destroy) # Make the button run the destroy function when clicked
+        
+        fixed = gtk.Fixed()
+        fixed.put(self.button1, 300, 30)
+        
+        self.window.add(fixed) # Add the fixed object (containing the button) to the window
+        self.window.show_all() # Show the window and all widgets
         self.window.connect("destroy", self.destroy) # Connect the close button to our newly created destroy function above to allow the program to be able to quit it's own process
     def main(self):
         gtk.main() # Main GTK Process
@@ -27,3 +34,7 @@ class Base:
 if __name__ == "__main__":
     base = Base() # Set up the Base class to the base object
     base.main() # Run the main function in the Base class
+else:
+    print "I need to be run by myself please, I will now quit this application"
+    quit()
+    # If not running by itself, display a message, and close the program
